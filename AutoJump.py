7d11de0623630b98a6428064d0cf40425035a78c
Jui-Shan Liang <jenny@jsliang.com>
2013-02-25 02:37:22 +0800

    Add filepath_filters settings

--- AutoJump.py
+++ AutoJump.py
@@ -56,6 +56,16 @@ def purge_autojump_database():
   # Remove entries that no longer exist
   run_shell_cmd('autojump --purge')
 
+def load_setting(view, setting_name, default_value=None):
+    if len(setting_name) < 1:
+        if default_value:
+            return default_value
+        return None
+
+    global_settings = sublime.load_settings("AutoJump.sublime-settings")
+
+    return view.settings().get(setting_name, global_settings.get(setting_name, default_value))
+
 class AutojumpLoadDatabaseCommand(sublime_plugin.WindowCommand):
   def on_done(self, picked):
     open_path = os.path.join(self.picked_folder, self.file_list[picked])
@@ -71,12 +81,24 @@ class AutojumpLoadDatabaseCommand(sublime_plugin.WindowCommand):
       purge_autojump_database()
       return
 
+    view = self.window.active_view()
+    filepath_filters = load_setting(view, "filepath_filters", None)
+
     self.file_list = []
     for dirpath, dirnames, filenames in os.walk(self.picked_folder, followlinks=True):
       for filename in filenames:
-          path_str = os.path.join(dirpath, filename)
-          path_str = path_str.replace(self.picked_folder + '/', '')
-          self.file_list.append(path_str)
+        path_str = os.path.join(dirpath, filename)
+        path_str = path_str.replace(self.picked_folder + '/', '')
+
+        ignore = False
+        for filepath_filter in filepath_filters:
+          if re.search(filepath_filter, path_str):
+            ignore = True
+
+        if ignore:
+          continue
+
+        self.file_list.append(path_str)
 
     if len(self.file_list) > 0:
       self.window.show_quick_panel(self.file_list, self.on_done)
