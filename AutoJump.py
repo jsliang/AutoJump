241dfa88dc5e3f3a08e76dd22e1fc78646fe268e
Jui-Shan Liang <jenny@jsliang.com>
2013-02-25 03:07:41 +0800

    Rename keep_autojump_database_intact to update_autojump_database

--- AutoJump.py
+++ AutoJump.py
@@ -124,13 +124,13 @@ class AutojumpLoadDatabaseCommand(sublime_plugin.WindowCommand):
 
 class AutojumpUpdateDatabase(sublime_plugin.EventListener):
   def on_load(self, view):
-    keep_autojump_database_intact = load_setting(view, "keep_autojump_database_intact", False)
-    if not keep_autojump_database_intact:
+    update_autojump_database = load_setting(view, "update_autojump_database", True)
+    if update_autojump_database:
       path = os.path.dirname( view.file_name() )
       add_to_autojump_database(path)
 
   def on_post_save(self, view):
-    keep_autojump_database_intact = load_setting(view, "keep_autojump_database_intact", False)
-    if not keep_autojump_database_intact:
+    update_autojump_database = load_setting(view, "update_autojump_database", True)
+    if update_autojump_database:
       path = os.path.dirname( view.file_name() )
       add_to_autojump_database(path)
